4c73924955f505ef6fa5733182b1ec4b6dc302a1
Adam Pod <adam.podoxin@gmail.com>
2019-08-12 23:18:34 -0700

    Create player movement and camera look

---
+++ Assets/Scripts/Player/CameraLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float cameraSensitivity = 3f;
+
+    public Transform cam;
+
+    private Vector3 cameraLook;
+
+    private void Update()
+    {
+        cameraLook += new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0) * cameraSensitivity;
+
+        if (cameraLook.x > 90) cameraLook.x = 90;
+        else if (cameraLook.x < -90) cameraLook.x = -90;
+
+        transform.eulerAngles = new Vector3(0, cameraLook.y, 0);
+        cam.eulerAngles = cameraLook;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Vector3 movement;
+
+    private void FixedUpdate()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
